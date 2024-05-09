0b9a22c8f031e269746b5abe02a422227e04a0c1
林郁盛 <sss890925@gmail.com>
2024-05-09 17:37:03 +0800

    fix: Add the feature to add multiple images and adjust image position

--- public/index.js
+++ public/index.js
@@ -6,30 +6,40 @@
     document.body.innerHTML += '';
     document.body.innerHTML += someUtils();
 
+    // 新增貼文區加入照片
     document.querySelector('#chooseImage-btn').addEventListener('click', (event) => {
-        // 取消點擊事件的默認行為
         event.preventDefault();
-        // 觸發 input 元素的點擊事件
         document.querySelector('#fileInput').click();
     });
 
-    // 監聽 input 元素的 change 事件
     document.querySelector('#fileInput').addEventListener('change', (event) => {
         const input = event.target;
         if (input.files && input.files.length > 0) {
-            // 遍歷選擇的文件
             for (let i = 0; i < input.files.length; i += 1) {
                 const file = input.files[i];
-                // 如果是圖片文件，執行相應操作
                 if (file.type.match('image.*')) {
                     const img = document.createElement('img');
                     img.src = URL.createObjectURL(file);
-                    document.querySelector('#selectedImage').appendChild(img);
+
+                    const closeBtn = document.createElement('div');
+                    closeBtn.classList.add('close-btn');
+                    closeBtn.textContent = '✖️';
+
+                    const imageContainer = document.createElement('div');
+                    imageContainer.classList.add('image-container');
+
+                    imageContainer.appendChild(img);
+                    imageContainer.appendChild(closeBtn);
+
+                    document.querySelector('#selectedImage').appendChild(imageContainer);
+
                     document.querySelector('#selectedImage').style.marginBottom = '10px';
                     document.querySelector('#selectedImage').style.display = 'block';
                 }
             }
         }
     });
+
+    // 取消已選擇的照片
 
 })();
